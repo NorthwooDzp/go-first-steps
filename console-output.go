ca54fe62c6895b04b0ee60e4d3f0f019144ab97e
Volodymyr L <northwoodzp@gmail.com>
2026-05-14 22:02:54 +0400

    outputs and formatting start

--- console-output.go
+++ console-output.go
@@ -1,5 +1,11 @@
 package main
 
-func consoleOutput() {
+import "fmt"
 
+func consoleOutput() {
+	fmt.Print("anything", "something else")                   // Print something without formatting (plain output)
+	fmt.Println("printing new line", "new value after space") // Print formatted (space between arguments) with line ending
+
+	// Formatting to specified format
+	fmt.Printf("%T", false) // Print member type
 }
